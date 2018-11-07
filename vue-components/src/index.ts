74052934f2ddb98ff8cd5a5dd1553ecce051b4e0
Adam Saunders <saunders@cs.umass.edu>
2018-11-07 11:25:10 -0500

    filtering engine improvements

--- vue-components/src/index.ts
+++ vue-components/src/index.ts
@@ -1,38 +1,10 @@
-// import FilterWidget from "./components/filter-engine/filter-engine";
-
-// import {
-//   FilterEngineState
-// } from './filter-engine/filter-engine-state';
-
-// import {
-//   // Candidate,
-//   CandidateGroup,
-//   CandidateGroups,
-//   KeyedRecord,
-//   KeyedRecordGroup,
-//   SelectionFilteringEngine
-// } from './filter-engine/FilterEngine';
-
-// const components = {
-//   FilterWidget
-//   // FilterEngineState
-// };
-
-// export interface Candidate;
-// export interface CandidateGroup;
-// export interface CandidateGroups;
-// export interface KeyedRecord;
-// export interface KeyedRecordGroup;
-// export interface FilterEngineState;
-
-// export default components;
 
 export {
   default as FilterWidget,
-} from "./src/components/filter-engine/filter-engine.vue";
+} from "./components/filter-engine/filter-engine.vue";
 
 export {
   SelectionFilteringEngine,
-} from "./src/components/filter-engine/FilterEngine";
+} from "./components/filter-engine/FilterEngine";
 
-export * from "./src/components/filter-engine/filter-engine-state";
+export * from "./components/filter-engine/filter-engine-state";
