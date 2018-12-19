1d1a1f3669a27ec28da39c27ca3ff9677e3a1631
Adam Saunders <saunders@cs.umass.edu>
2018-12-19 14:29:31 -0500

    updated deps

--- packages/vue-components/src/components/graph-paper/graph-paper.stories.ts
+++ packages/vue-components/src/components/graph-paper/graph-paper.stories.ts
@@ -1,9 +1,8 @@
 import {storiesOf} from "@storybook/vue";
 
 import GraphPaperDev from "./graph-paper.dev.vue";
-// import GraphPaper from "./graph-paper.vue";
 
-import store from "../../store";
+import store from "@/store";
 
 storiesOf("GraphPaperDev", module).add("basic", () => ({
   store,
--- packages/vue-components/src/stories/pdf-text.stories.ts
+++ packages/vue-components/src/stories/pdf-text.stories.ts
@@ -1,10 +1,11 @@
+
 import {storiesOf} from "@storybook/vue";
 
-import FilterEngineDev from "./filter-engine.dev.vue";
+import FilterEngineDev from "@/components/filter-engine/filter-engine.dev.vue";
 
 import store from "@/store";
 
-storiesOf("FilterWidget", module).add("basic", () => ({
+storiesOf("PDF + Text", module).add("basic", () => ({
   store,
   components: {FilterEngineDev},
   template: "<FilterEngineDev />",
