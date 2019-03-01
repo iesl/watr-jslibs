e88e8c043b5c04683a701f07c1d5c7ec792d05f1
Adam Saunders <saunders@cs.umass.edu>
2019-03-01 16:16:49 -0500

    server wip

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,100 +1,105 @@
 
-/* global require __dirname */
-
-// const options = require('commander');
-// import Koa from 'koa';
-// const Koa = require('koa');
-
-
-const Router = require('koa-router');
-const render = require('koa-ejs');
-const path = require('path');
-const send = require('koa-send');
-const json = require('koa-json');
-
 import Koa, { Context } from 'koa';
-// const webpackDevMiddleware = require('webpack-dev-middleware');
-// const webpack = require('webpack');
-// const config = require('../../webpack.config');
-// const compiler = webpack(config);
-// import { RouterContext } from 'Router';
+import  Router from 'koa-router';
+import  render from 'koa-ejs';
+import  path   from 'path';
+import  send   from 'koa-send';
+import  json   from 'koa-json';
+import opts from 'commander';
+
 
 const router = new Router();
 const app = new Koa();
 
-let projectRoot = path.resolve(__dirname, '../..' );
-let srcRoot = path.resolve(projectRoot, 'src');
-let devRoot = path.resolve(srcRoot, 'dev');
-let devDataRoot = path.resolve(devRoot, 'data');
-let srcClientRoot = path.resolve(srcRoot, 'client');
-let distRoot = path.resolve(projectRoot, 'dist');
-let projectParent = path.resolve(projectRoot, '..');
-let watrmarksLib = path.resolve(projectParent, './watr-marks/js/target/scala-2.12');
+const projectRoot = path.resolve(__dirname, '../..' );
+// const srcRoot = path.resolve(projectRoot, 'src');
+// const devRoot = path.resolve(srcRoot, 'dev');
+// const devDataRoot = path.resolve(devRoot, 'data');
+// const srcClientRoot = path.resolve(srcRoot, 'client');
+const projectParent = path.resolve(projectRoot, '..');
+const watrmarksLib = path.resolve(projectParent, './watr-marks/js/target/scala-2.12');
 
-console.log('srcRoot', srcRoot);
-console.log('srcClientRoot', srcClientRoot);
-console.log('projectRoot', projectRoot);
-console.log('devData', devDataRoot);
+
+opts
+  .version('0.1.0')
+  .option('--corpus <path>', 'Path to corpus')
+  .option('--public <path>', 'Path public assets')
+  .parse(process.argv)
+;
+
+
+console.log('corpus root', opts.corpus);
+console.log('corpus root', opts.public);
+const distRoot =  opts.public;// path.resolve(projectRoot, 'dist');
+
+
+// console.log('srcRoot', srcRoot);
+// console.log('srcClientRoot', srcClientRoot);
+// console.log('projectRoot', projectRoot);
+// console.log('devData', devDataRoot);
 // console.log('config publicPath', config.output.path);
 
-render(app, {
-  root: path.join(devRoot, 'view'),
-  layout: 'template',
-  viewExt: 'html',
-  cache: false,
-  debug: false
-});
+// render(app, {
+//   root: path.join(devRoot, 'view'),
+//   layout: 'template',
+//   viewExt: 'html',
+//   cache: false,
+//   debug: false
+// });
 
 // Serve public assets
 
 router
-  .get('/dist/:file', async function(ctx: Context) {
+  .get('/:file', async function(ctx: Context) {
     await send(ctx, ctx.params.file, { root: distRoot  });
   })
-  .get('/view/:file', async function(ctx: Context) {
-    await send(ctx, ctx.params.file, { root: path.join(devRoot, 'view') });
+  .get('/fonts/:file', async function(ctx: Context) {
+    await send(ctx, ctx.params.file, { root: path.join(distRoot, 'fonts') });
+  })
+  .get('/dist/:file', async function(ctx: Context) {
+    await send(ctx, ctx.params.file, { root: distRoot  });
   })
   .get('/scalajs/:file', async function(ctx: Context) {
     await send(ctx, ctx.params.file, { root: watrmarksLib });
   })
-  .get('/client/lib/:file', async function(ctx: Context) {
-    await send(ctx, ctx.params.file, { root: path.join(srcClientRoot, 'lib') });
-  })
-  .get('/style/:file', async function(ctx: Context) {
-    await send(ctx, ctx.params.file, { root: srcRoot + '/style' });
-  })
-  .get('/image/page/:pagenum', async function(ctx: Context) {
-    let pagenum = ctx.params.pagenum;
-    let p = path.resolve(devDataRoot, `corpus-entry-0/page-images`);
-    let file = `page-${pagenum}.opt.png`;
-    await send(ctx, file, { root: p } );
-  })
-  .get('/data/textgrids/:num', async function(ctx: Context) {
-    let num = ctx.params.num;
-    let p = path.resolve(devDataRoot, `textgrids`);
-    let file = `textgrid-${num}.json`;
-    await send(ctx, file, { root: p  });
-  })
-  .get('/data/textgrid', async function(ctx: Context) {
-    let p = path.resolve(devDataRoot, `corpus-entry-0`);
-    let file = 'textgrid.json';
-    await send(ctx, file, { root: p  });
-  })
-  .get('/data/tracelog/:num', async function(ctx: Context) {
-    let num = ctx.params.num;
-    let p = path.resolve(devDataRoot, `tracelogs`);
-    let file = `tracelog-${num}.json`;
-    await send(ctx, file, { root: p  });
-  })
-  .get('/api/v1/corpus/artifacts/entry/:stableId/image/page/:pagenum' , async function(ctx: Context) {
-    let pagenum = ctx.params.pagenum;
-    let p = path.resolve(devDataRoot, `corpus-entry-0/page-images`);
-    let file = `page-${pagenum}.opt.png`;
-    await send(ctx, file, { root: p } );
-  })
-  .get('/api/v1/labeling/zones/undefined' , async function(ctx: Context) {
-    ctx.body = {};
-  })
+  // .get('/client/lib/:file', async function(ctx: Context) {
+  //   await send(ctx, ctx.params.file, { root: path.join(srcClientRoot, 'lib') });
+  // })
+  // .get('/style/:file', async function(ctx: Context) {
+  //   await send(ctx, ctx.params.file, { root: srcRoot + '/style' });
+  // })
+  // .get('/image/page/:pagenum', async function(ctx: Context) {
+  //   const pagenum = ctx.params.pagenum;
+  //   const p = path.resolve(devDataRoot, `corpus-entry-0/page-images`);
+  //   const file = `page-${pagenum}.opt.png`;
+  //   await send(ctx, file, { root: p } );
+  // })
+  // .get('/data/textgrids/:num', async function(ctx: Context) {
+  //   const num = ctx.params.num;
+  //   const p = path.resolve(devDataRoot, `textgrids`);
+  //   const file = `textgrid-${num}.json`;
+  //   await send(ctx, file, { root: p  });
+  // })
+  // .get('/data/textgrid', async function(ctx: Context) {
+  //   const p = path.resolve(devDataRoot, `corpus-entry-0`);
+  //   const file = 'textgrid.json';
+  //   await send(ctx, file, { root: p  });
+  // })
+  // .get('/data/tracelog/:num', async function(ctx: Context) {
+  //   const num = ctx.params.num;
+  //   const p = path.resolve(devDataRoot, `tracelogs`);
+  //   const file = `tracelog-${num}.json`;
+  //   await send(ctx, file, { root: p  });
+  // })
+  // .get('/api/v1/corpus/artifacts/entry/:stableId/image/page/:pagenum' , async function(ctx: Context) {
+  //   const pagenum = ctx.params.pagenum;
+  //   const p = path.resolve(devDataRoot, `corpus-entry-0/page-images`);
+  //   const file = `page-${pagenum}.opt.png`;
+  //   await send(ctx, file, { root: p } );
+  // })
+  // .get('/api/v1/labeling/zones/undefined' , async function(ctx: Context) {
+  //   ctx.body = {};
+  // })
 ;
 
 
@@ -102,11 +107,10 @@ function run () {
 
   router
     .get('/', async function (ctx: Context) {
-      await ctx.render('index', {});
+      await send(ctx, 'index.html', { root: distRoot  });
     })
-    .get('/:page', async function (ctx: Context) {
-      let { page: page } = ctx.params;
-      await ctx.render(page, {});
+    .get('/app.js', async function (ctx: Context) {
+      await send(ctx, 'app.js', { root: distRoot  });
     })
   ;
 
