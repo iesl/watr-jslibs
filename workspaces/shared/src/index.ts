7fb8fa817ad3a9513de0ed10e7241e84a60a83cc
Adam Saunders <saunders@cs.umass.edu>
2019-12-05 12:04:39 -0500

    refactor pdf page (wip)

--- workspaces/shared/src/index.ts
+++ workspaces/shared/src/index.ts
@@ -9,7 +9,6 @@ import * as LodashPlus from "./LodashPlus";
 import * as MouseHandlerSets from "./MouseHandlerSets";
 export * from './MouseHandlerSets';
 
-import * as SplitWin from "./SplitWin";
 import * as tstags from "./tstags";
 
 import * as utils from "./utils";
@@ -17,10 +16,6 @@ export * from "./utils";
 
 import * as d3x from "./d3-extras";
 
-// import * as DataTypes from "./DataTypes";
-// export * from './DataTypes';
-
 import * as GridTypes from "./TextGridTypes";
-// export * from './TextGridTypes';
 
-export { coords, colors, LodashPlus, MouseHandlerSets, SplitWin, tstags, utils, GridTypes, d3x };
+export { coords, colors, LodashPlus, MouseHandlerSets, tstags, utils, GridTypes, d3x };
---
+++ workspaces/ui-components/src/components/pdf-page/index.vue
@@ -0,0 +1,3 @@
+<template lang="html" src="./pdf-page.html"></template>
+<script lang="ts" src="./pdf-page.ts"></script>
+<style lang="scss" src="./pdf-page.scss" scoped></style>
