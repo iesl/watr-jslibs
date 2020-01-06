8e1d3eee81c13dde318bf92177f602ba4d0bd4d3
Adam Saunders <saunders@cs.umass.edu>
2020-01-06 10:04:13 -0500

    removing pixi.js

--- workspaces/ui-components/src/components/compositions/__stories__/drawto/drawto-canvas-story/index.vue
+++
@@ -1,7 +0,0 @@
-
-<template lang="html" src="./drawto-canvas-story.html"></template>
-<script lang="ts" src="./drawto-canvas-story.ts">
-// story-name=drawtoCanvas
-// story-title=Canvas/WebGL Drawing (pixi.js)
-</script>
-<style lang="scss" src="./drawto-canvas-story.scss" scoped></style>
---
+++ workspaces/ui-components/src/components/compositions/__stories__/drawto/svg-drawto-story/index.vue
@@ -0,0 +1,7 @@
+
+<template lang="html" src="./svg-drawto-story.html"></template>
+<script lang="ts" src="./svg-drawto-story.ts">
+// story-name=drawtoSvg
+// story-title=SVG Drawing Primitives
+</script>
+<style lang="scss" src="./svg-drawto-story.scss" scoped></style>
