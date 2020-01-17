5d85910e9ec8283dde76a1513a189b9b55adbfe2
Adam Saunders <saunders@cs.umass.edu>
2020-01-17 14:47:35 -0500

    textworks-aas: init

--- workspaces/textworks-aas/src/main.ts
+++ workspaces/textworks-aas/src/main.ts
@@ -1,1 +1,31 @@
 //
+import _ from 'lodash';
+import fs from 'fs-extra';
+import csv from 'fast-csv';
+import path from 'path';
+
+import cmds from 'caporal';
+const program = cmds;
+
+program
+  .command('load', 'load csv and report some info')
+  .argument('<file>', 'csv file name')
+  .action((args, _opts, _logger) => {
+
+    loadcsv(args.file)
+
+  });
+
+function loadcsv(csvfile: string) {
+
+  const csvabs = path.resolve(csvfile);
+  const fileExists = fs.existsSync(csvabs);
+
+  console.log(`exists=${fileExists}: ${csvabs} `);
+
+  fs.createReadStream(csvabs)
+    .pipe(csv.parse({ headers: true }))
+    .on('data', row => {
+      console.log('> ', row);
+    });
+}
