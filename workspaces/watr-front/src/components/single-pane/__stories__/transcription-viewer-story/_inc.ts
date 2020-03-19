36e2557e881a0d1749a042ed941d13562e6a4530
Adam Saunders <saunders@cs.umass.edu>
2020-03-19 11:06:57 -0400

    starting transcription viewer code

---
+++ workspaces/watr-front/src/components/single-pane/__stories__/transcription-viewer-story/_inc.ts
@@ -0,0 +1,35 @@
+import { usePdfTextViewer } from '~/components/single-pane/pdf-text-viewer'
+import { defineComponent } from '@vue/composition-api';
+import * as GridTypes from '~/lib/TextGridTypes';
+import * as coords from '~/lib/coord-sys';
+import { getArtifactData } from '~/lib/axios';
+import { initState } from '~/components/basics/component-basics';
+import { divRef } from '~/lib/vue-composition-lib';
+
+export default defineComponent({
+  components: {},
+  setup() {
+
+    const state = initState();
+    const mountPoint = divRef();
+
+    usePdfTextViewer({ mountPoint, state }).then(pdfTextViewer => {
+
+    const { setText } = pdfTextViewer;
+
+    const entryId = '1503.00580.pdf.d';
+
+    getArtifactData(entryId, 'textgrid')
+      .then((grid: GridTypes.Grid) => {
+        const page0 = grid.pages[0]
+        const textgrid = page0.textgrid;
+        const pageBounds = coords.mk.fromLtwh(20, 20, 0, 0);
+        setText({ textgrid, pageBounds });
+      });
+    });
+
+    return {
+      mountPoint
+    };
+  }
+});
